e9ad72ef2718b47eb42c59f7ae9b8b435316080d
Hugo Figueroa <hugofiguer97@gmail.com>
2020-11-14 14:58:09 -0600

    Servicio de rabbitmq en python

--- serverpython/app.py
+++ serverpython/app.py
@@ -10,7 +10,7 @@ r = redis.Redis(host='35.192.86.242', port=6379) # Dbredis
 
 def main():
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq-service'))
     channel = connection.channel()
 
     channel.queue_declare(queue='covid-cases')
